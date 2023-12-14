4b2a0b6d81c6e7cf15384912cf4c842478f5059d
Jongwook-Seong <ypwa121@gmail.com>
2023-12-14 22:32:39 +0900

    feat: AWS S3 연결 설정 및 파일 업로드 준비

--- src/main/java/com/goorm/devlink/profileservice/controller/ProfileController.java
+++ src/main/java/com/goorm/devlink/profileservice/controller/ProfileController.java
@@ -1,8 +1,10 @@
 package com.goorm.devlink.profileservice.controller;
 
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.goorm.devlink.profileservice.dto.ProfileDto;
 import com.goorm.devlink.profileservice.entity.ProfileType;
 import com.goorm.devlink.profileservice.service.ProfileService;
+import com.goorm.devlink.profileservice.util.AwsS3Uploader;
 import com.goorm.devlink.profileservice.vo.ProfileCommentResponse;
 import com.goorm.devlink.profileservice.vo.ProfileCreateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
 
 @RestController
 public class ProfileController {
@@ -33,7 +44,19 @@ public class ProfileController {
 
     /** 마이프로필 편집 **/
     @PostMapping("/api/myprofile")
-    public ResponseEntity<ProfileCommentResponse> createMyProfile(@RequestBody ProfileCreateRequest profileCreateRequest, @RequestHeader("userUuid") String userUuid) {
+    public ResponseEntity<ProfileCommentResponse> createMyProfile(@RequestBody ProfileCreateRequest profileCreateRequest, @RequestHeader("userUuid") String userUuid) throws IOException {
+
+        //===== 파일 업로드 테스트 =====//
+//        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
+//        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+//        try {
+//            s3.putObject(bucket_name, key_name, new File(file_path));
+//        } catch (AmazonServiceException e) {
+//            System.err.println(e.getErrorMessage());
+//            System.exit(1);
+//        }
+
+
         ProfileDto profileDto = ProfileDto.getInstanceForCreate(profileCreateRequest, userUuid);
         String profileUuid = profileService.createProfile(profileDto);
         return new ResponseEntity<>(ProfileCommentResponse.getInstanceForCreate(profileUuid), HttpStatus.CREATED);
