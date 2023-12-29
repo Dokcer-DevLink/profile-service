cbce37d514f03a2eb25432ef555e1af2e5c21de6
Jongwook-Seong <ypwa121@gmail.com>
2023-12-29 12:36:02 +0900

    refactor: AmazonS3 대신 AmazonS3Client로 사용

--- src/main/java/com/goorm/devlink/profileservice/service/S3UploadService.java
+++ src/main/java/com/goorm/devlink/profileservice/service/S3UploadService.java
@@ -1,14 +1,11 @@
 package com.goorm.devlink.profileservice.service;
 
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.goorm.devlink.profileservice.util.UploadFileGenerator;
-import com.goorm.devlink.profileservice.vo.UploadFile;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -18,13 +15,12 @@ import java.io.InputStream;
 @RequiredArgsConstructor
 public class S3UploadService {
 
-    private final AmazonS3 amazonS3;
-    private final UploadFileGenerator uploadFileGenerator;
+    private final AmazonS3Client amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    public String saveFile(String encodingImage, String userUuid) throws IOException {
+    public String saveFile(String encodingImage, String userUuid) {
 
         byte[] decodedImage = Base64.decodeBase64(encodingImage);
         InputStream bis = new ByteArrayInputStream(decodedImage);
@@ -35,21 +31,10 @@ public class S3UploadService {
 
         String fileName = "profile/"+ userUuid;
 
-        if (amazonS3.doesObjectExist(bucketName, fileName))
-            amazonS3.deleteObject(bucketName, fileName);
+        if (amazonS3Client.doesObjectExist(bucketName, fileName))
+            amazonS3Client.deleteObject(bucketName, fileName);
 
-        amazonS3.putObject(bucketName, fileName, bis, metadata);
-        return amazonS3.getUrl(bucketName, fileName).toString();
-        /////
-
-//        ObjectMetadata metadata = new ObjectMetadata();
-//        metadata.setContentLength(multipartFile.getSize());
-//        metadata.setContentType(multipartFile.getContentType());
-//
-//        UploadFile uploadFile = uploadFileGenerator.generate(multipartFile);
-//        String storeFilename = uploadFile.getStoreFilename();
-//
-//        amazonS3.putObject(bucketName, storeFilename, multipartFile.getInputStream(), metadata);
-//        return amazonS3.getUrl(bucketName, storeFilename).toString();
+        amazonS3Client.putObject(bucketName, fileName, bis, metadata);
+        return amazonS3Client.getUrl(bucketName, fileName).toString();
     }
 }
